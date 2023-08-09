5fb99e59a9ed596c1be69e2a8cef2d8244018566
Emmaalbelo <emmaalbelo@gmail.com>
2023-08-09 19:02:20 +0200

    first

---
+++ packages/js/traslate.js
@@ -0,0 +1,171 @@
+// Traducciones en español
+const translations = {
+  // HOME
+  greeting: "Hola!",
+  Descripcion: "Mi enfoque como desarrollador se caracteriza por mi dedicación autodidacta, mi atención meticulosa a los detalles y mi búsqueda constante de la perfección en mi trabajo",
+  // ABOUT
+  AboutMe: "Sobre mi",
+  about__description: "Soy un profesional autodidacta con una sólida formación en desarrollo web, especializado tanto en el frontend como en el backend. Especializado en Python y sus frameworks, con experiencia en HTML, CSS, JavaScript y manejo de bases de datos SQL. Interesado en buenas prácticas y en constante capacitación para aprender nuevas tecnologías. Orientado a la resolución de problemas y dispuesto a enfrentar desafíos en proyectos innovadores. Considero que mi enfoque metódico y mi pasión por la programación son activos valiosos que puedo aportar a cualquier proyecto.",
+  Download: "Descargar CV",
+  also: "Además, me pueden encontrar aquí.",
+  // SKILLS
+  technical_level: "Mi nivel tecnico",
+  skills__title: 'Desarrollador de software',
+  exp_time: 'Mas de 1 año',
+  Frameworks: 'Frameworks populares de Python',
+  Data_Analyst: 'Analista de datos',
+  exp_time_Data: 'Mas de 1 año',
+  Web_Developer: 'Desarrollador Web',
+  exp_time_Web: 'Mas de 1 año',
+  // QUALIFICATION
+  Qualifications: "Formación y Experiencia profecional",
+  journey: 'Mi recorrido personal',
+  Education: 'Educación',
+  Work: 'Trabajo',
+  qualification1: 'Curso Maestro Python',
+  // SERVICE
+  Services: 'Servicios',
+  offer: 'Lo que ofrezco',
+  Software_developer: 'Desarrollador de software',
+  Viewmore1: 'Ver mas',
+  Sof_developer: 'Desarrollador de software',
+  servSoft1: 'Desarrollo de aplicaciones backend.',
+  servSoft2: 'Implementar servicios web y APIs.',
+  servSoft3: 'Mantenimiento y depuración de código.',
+  servSoft4: 'Diseño e implementación de interfaces gráficas de escritorio.',
+  servSoft5: 'Asegurar la seguridad y el rendimiento del sistema.',
+  Data_Analyst1: 'Analista de datos',
+  DataAnalyst: 'Analista de datos',
+  servData1: 'Analizar sistemas de datos.',
+  servData2: 'Automatizar la recuperación de información.',
+  servData3: 'Aplicar sistemáticamente técnicas estadísticas y lógicas.',
+  servData4: 'Visualizar gráficos, diagramas y preparar informes y paneles de control.',
+  servData5: 'Gestionar bases de datos.',
+  Viewmore2: 'Ver mas',
+  WebDeveloper1: 'Desarrollador Web',
+  WebDeveloper2: 'Desarrollador Web',
+  Viewmore3: 'Ver mas',
+  servWeb1: 'Desarrollo la interfaz de usuario.',
+  servWeb2: 'Desarrollo de páginas web.',
+  servWeb3: 'Diseño web adaptable',
+  servWeb4: 'Diseño en WordPress.',
+  // PORTFOLIO
+  Portfolio: 'Portafolio',
+  MostRecent: 'Trabajos mas recientes',
+  port1_desc: 'Utilizado para dar estilo y dar vida al sitio web, aplicando técnicas de diseño modernas como la capacidad de respuesta y animaciones sutiles.',
+  Python_port: 'Aplicacion de escritorio con Python',
+  port2_desc: 'Diseñado para gestionar estudiantes, conectado a una base de datos. Proporciona una interfaz fácil de usar para realizar diversas tareas relacionadas con la gestión de estudiantes, como agregar nuevos estudiantes, actualizar su información y generar informes.',
+  Python_Backend: 'Python Backend / FastAPI y SQL',
+  port3_desc: 'La combinación de FastAPI y una base de datos SQL permite desarrollar programas web eficientes con operaciones CRUD. FastAPI proporciona una sintaxis fácil de usar para definir rutas y modelos de datos, mientras que las bases de datos SQL permiten almacenar y recuperar datos de manera eficiente',
+  // CONTACT ME
+  project: '¿Buscas un profesional para tu equipo?',
+  ContactMe: 'Contactame',
+  Call: 'Telefono',
+  Location: 'Localización',
+  Cont_hours: 'Horario de contacto',
+  Send: 'Enviar mensaje',
+  // FOOTER
+  ServicesFoot: 'Servicios',
+  PortfolioFoot: 'Portafolio',
+  ContactFoot: 'Contacto',
+  footer__coded: 'Realizado por Emmanuel Albelo',
+  // TIPING
+  profesiones: ["Emmanuel", "desarrollador web", "desarrollador backend", "desarrollador frontend", "desarrollador fullstack"],
+};
+
+const originalTexts = {
+  // HOME
+  greeting: "Hello!",
+  Descripcion: "My approach as a developer is characterized by my self-taught dedication, my meticulous attention to detail, and my constant pursuit of perfection in my work.",
+  // ABOUT
+  AboutMe: "About Me",
+  about__description: "I am a self-taught professional with a strong background in web development, specializing in both frontend and backend. I have expertise in Python and its frameworks, along with experience in HTML, CSS, JavaScript, and SQL database management. I am interested in adhering to best practices and continuously learning new technologies. I am problem-solving oriented and ready to take on challenges in innovative projects. I believe that my methodical approach and passion for programming are valuable assets that I can contribute to any project.",
+  Download: "Download CV",
+  also: "Also, I can be found here",
+  // SKILLS
+  technical_level: "My technical level",
+  skills__title: 'Software developer',
+  exp_time: 'More than 1 year',
+  Frameworks: 'Popular Python Frameworks',
+  Data_Analyst: 'Data Analyst',
+  exp_time_Data: 'More than 1 year',
+  Web_Developer: 'Web Developer',
+  exp_time_Web: 'More than 1 year',
+  // QUALIFICATION
+  Qualifications: "Qualifications and Experience",
+  journey: 'My personal journey',
+  Education: 'Education',
+  Work: 'Work',
+  qualification1: 'Python Master Course',
+  // SERVICE
+  Services: 'Services',
+  offer: 'What I offer',
+  Software_developer: 'Software Developer',
+  Viewmore1: 'View more',
+  Sof_developer: 'Software Developer',
+  servSoft1: 'Development of backend applications.',
+  servSoft2: 'Implementation of web services and APIs.',
+  servSoft3: 'Maintenance and debugging of code.',
+  servSoft4: 'Design and implementation of desktop graphical interfaces.',
+  servSoft5: 'Ensuring system security and performance.',
+  Data_Analyst1: 'Data Analyst',
+  DataAnalyst: 'Data Analyst',
+  servData1: 'Analyze data systems.',
+  servData2: 'Automating information retrieval.',
+  servData3: 'Systematically applying statistical and logical techniques.',
+  servData4: 'Visualizing graphs, charts and preparing reports and dashboards.',
+  servData5: 'Manage databases.',
+  Viewmore2: 'View more',
+  WebDeveloper1: 'Web Developer',
+  WebDeveloper2: 'Web Developer',
+  Viewmore3: 'View more',
+  servWeb1: 'I develop the user interface.',
+  servWeb2: 'Webpage development.',
+  servWeb3: 'Responsive web design.',
+  servWeb4: 'Design in WordPress.',
+  // PORTFOLIO
+  Portfolio: 'Portfolio',
+  MostRecent: 'Most recent works',
+  port1_desc: 'Used to style and bring life to the website, applying modern design techniques such as responsiveness and subtle animations.',
+  Python_port: 'Python desktop app',
+  port2_desc: 'Designed to manage students, connected to a database. It provides a user-friendly interface for performing various tasks related to student management, such as adding new students, updating their information, and generating reports.',
+  Python_Backend: 'Python Backend / FastAPI and SQL',
+  port3_desc: 'The combination of FastAPI and SQL database allows for the development of efficient web programs with CRUD operations. FastAPI provides an easy-to-use syntax for defining routes and data models, while SQL databases enable efficient storage and retrieval of data.',
+  project: 'Are you looking for a professional for your team?',
+  // CONTACT ME
+  ContactMe: 'Contact me',
+  Call: 'Call me',
+  Location: 'Localization',
+  Cont_hours: 'Contact hours',
+  Send: 'Send message',
+  // FOOTER
+  ServicesFoot: 'Services',
+  PortfolioFoot: 'Portfolio',
+  ContactFoot: 'Contact',
+  footer__coded: 'Coded by Emmanuel Albelo',
+
+};
+
+let isTranslated = false;
+
+function changelanguage() {
+  const btnCambiar = document.getElementById("btn-cambiar");
+
+  Object.keys(translations).forEach(function (id) {
+    const elemento = document.getElementById(id);
+    if (elemento) {
+      if (isTranslated) {
+        elemento.innerText = originalTexts[id];
+        btnCambiar.innerHTML = '<img class="flag-icon" src="packages/images/buttons/ingles.png" alt="English">';
+      } else {
+        elemento.innerText = translations[id];
+        btnCambiar.innerHTML = '<img class="flag-icon" src="packages/images/buttons/espanol.png" alt="Español">';
+      }
+    }
+  });
+
+  isTranslated = !isTranslated;
+}
+
+// Call changelanguage() function on page load
+window.addEventListener("DOMContentLoaded", changelanguage);
